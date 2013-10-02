515a76d708c64d8af60d227d215031f7a273f6cd
Dave Andersen <dga@cs.cmu.edu>
2013-10-01 20:41:43 -0400

    Saving state again.  Basic insertion and retrieval works, cuckooing does not -- BFS not yet implemented.

--- cuckoo.go
+++ cuckoo.go
@@ -24,15 +24,17 @@ import (
 )
 
 const (
+	SLOTS_PER_BUCKET = 4 // This is kinda hardcoded all over the place
 	DEFAULT_START_POWER = 16 // 2^16 keys to start with.
 	N_LOCKS = 2048
+	MAX_REACH = 500 // number of buckets to examine before full
 )
 
 type keytype string
 type valuetype string
 
 type kvtype struct {
-	keyhash int64
+	keyhash uint64
 	key *keytype
 	value *valuetype
 }
@@ -52,13 +54,16 @@ func NewTable() *Table {
 }
 
 func (t *Table) sizeTable(twopower uint) {
-	t.hashpower = twopower
-	t.bucketMask = (1<<twopower)-1
+	t.hashpower = twopower-2
+	t.bucketMask = (1<<t.hashpower)-1
 }
 
 func NewTablePowerOfTwo(twopower uint) *Table {
 	t := &Table{}
 	t.sizeTable(twopower)
+	// storage holds items, but is organized into N/4 fully
+	// associative buckets conceptually, so the hashpower differs
+	// from the storage size.
 	t.storage = make([]kvtype, 1<<twopower)
 	t.cheatmap = make(map[keytype]valuetype)
 	t.crcTab = crc64.MakeTable(crc64.ECMA)
@@ -66,7 +71,7 @@ func NewTablePowerOfTwo(twopower uint) *Table {
 }
 
 func (t *Table) getKeyhash(k keytype) uint64 {
-	return crc64.Checksum([]byte(k), t.crcTab)
+	return ((1<<63) | crc64.Checksum([]byte(k), t.crcTab))
 }
 
 var _ = fmt.Println
@@ -78,16 +83,66 @@ func (t *Table) indexes(keyhash uint64) (i1, i2 uint64){
 	return
 }
 
+func (t *Table) tryBucketRead(k keytype, keyhash uint64, bucket uint64) (valuetype, bool) {
+	storageOffset := bucket*4
+	buckets := t.storage[storageOffset:storageOffset+4]
+	for _, b := range buckets {
+		if b.keyhash == keyhash {
+			if *b.key == k {
+				return *b.value, true
+			}
+		}
+	}
+	return valuetype(0), false
+}
+
+func (t Table)hasSpace(bucket uint64) (bool, int) {
+	storageOffset := bucket*4
+	buckets := t.storage[storageOffset:storageOffset+4]
+	for i, b := range buckets {
+		if b.keyhash == 0 {
+			return true, i
+		}
+	}
+	return false, 0
+}
+
+func (t Table) insert(k keytype, v valuetype, keyhash uint64, bucket uint64, slot int) {
+	b := &(t.storage[bucket*4+uint64(slot)])
+	b.keyhash = keyhash
+	b.key = &k
+	b.value = &v
+}
+
 func (t *Table) Get(k keytype) (v valuetype, found bool) {
-	v, found = t.cheatmap[k]
 	keyhash := t.getKeyhash(k)
 	i1, i2 := t.indexes(keyhash)
-	return
-	found = true
+	v, found = t.tryBucketRead(k, keyhash, i1)
+	if (!found) {
+		v, found = t.tryBucketRead(k, keyhash, i2)
+	}
 	return
 }
 
+// func (t *Table) slotSearchBFS(i1, i2 uint64) {
+// 	reach := 0
+// 	var path [4]uint64 // List of actual storage offsets
+// 	for depth := 0; depth < 4; depth++ {
+// 		if hasit, where := t.hasSpace(i1); hasit {
+			
+// 	}
+// }
+
 func (t *Table) Put(k keytype, v valuetype) error {
-	t.cheatmap[k] = v
+	keyhash := t.getKeyhash(k)
+	i1, i2 := t.indexes(keyhash)
+	if hasSpace, where := t.hasSpace(i1); hasSpace {
+		t.insert(k, v, keyhash, i1, where)
+	} else if hasSpace, where := t.hasSpace(i2); hasSpace {
+		t.insert(k, v, keyhash, i2, where)
+	} else {
+			//path := t.slotSearchBFS(i1, i2)
+		panic("Have to cuckoo, but this isn't implemented yet!")
+	}
 	return nil
 }
--- cuckoo_test.go
+++ cuckoo_test.go
@@ -31,5 +31,26 @@ func TestBasic(t *testing.T) {
 			t.Fatalf("Wrong value, expected testvalue-%d got %s", i, vs)
 		}
 	}
+}
 
+func TestFill(t *testing.T) {
+	ct := NewTablePowerOfTwo(10)
+	// Should be able to hold at least 960 elements, but will have to
+	// cuckoo a lot to fill those last bits.  Stress test the cuckooing.
+	limit := 960
+	for i := 0; i < limit; i++ {
+		istr := strconv.Itoa(i)
+		ct.Put(keytype(istr), valuetype(fmt.Sprintf("testvalue-%d", i)))
+	}
+	for i := 0; i < limit; i++ {
+		istr := strconv.Itoa(i)
+		v, found := ct.Get(keytype(istr))
+		if (!found) {
+			t.Fatal("Could not find key", istr)
+		}
+		vs := string(v)
+		if vs != fmt.Sprintf("testvalue-%d", i) {
+			t.Fatalf("Wrong value, expected testvalue-%d got %s", i, vs)
+		}
+	}
 }
